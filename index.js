cc0505171f9f592364e720b39c5990f6b1c8a8f7
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-19 14:25:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+  const foundItem = record.find((ele) => ele.result === "W");
+  if (foundItem) return foundItem.year;
+  return undefined;
+}
